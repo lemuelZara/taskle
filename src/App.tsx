310330b4ab1c30c68df388573ad6c061185c52c8
Lemuel Coelho Zara <lemuel.czara@gmail.com>
2024-06-18 22:42:01 -0300

    Main functionalities implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,102 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ChangeEvent, MouseEvent, useState } from 'react';
+
+import styles from './App.module.css';
+
+type Todo = {
+  id: number;
+  name: string;
+  isCompleted?: boolean;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState<Todo>({ id: 0, name: '', isCompleted: false });
+
+  function handleChangeTodo(event: ChangeEvent<HTMLInputElement>) {
+    setNewTodo({ id: todos.length + 1, name: event.target.value, isCompleted: false });
+  }
+
+  function handleCompletedTodo(event: ChangeEvent<HTMLInputElement>, id: number) {
+    const todoToBeCompleted = todos.map(t => {
+      if (t.id === id) {
+        return { ...t, isCompleted: event.target.checked };
+      }
+
+      return t;
+    })!;
+
+    setTodos(todoToBeCompleted);
+  }
+
+  function handleAddTodo(event: MouseEvent<HTMLButtonElement>) {
+    event.preventDefault();
+
+    setTodos((prevState) => [...prevState, newTodo!]);
+    setNewTodo({ id: 0, name: '' });
+  }
+
+  function handleDeleteTodo(event: MouseEvent<HTMLButtonElement>, id: number) {
+    event.preventDefault();
+
+    const todosWithoutDeletedOne = todos.filter(t => t.id !== id);
+
+    setTodos(todosWithoutDeletedOne);
+  }
+
+  const isNewTodoEmpty = newTodo.name.length === 0;
+
+  const completedTodos = todos.filter(t => t.isCompleted).length;
+
+  function renderCompletedTextTodos() {
+    if (completedTodos === 0) {
+      return <p>Concluídas - <span>{completedTodos}</span></p>
+    }
+
+    return <p>Concluídas - <span>{completedTodos} de {todos.length}</span></p>
+  }
 
   return (
-    <>
+    <main className={styles.main}>
+      <header>
+        Logo
+      </header>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="text" placeholder="Adicione uma nova tarefa" onChange={handleChangeTodo} value={newTodo.name} />
+        <button onClick={handleAddTodo} disabled={isNewTodoEmpty}>Criar</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <div>
+          <p>Tarefas criadas - <span>{todos.length}</span></p>
+          {renderCompletedTextTodos()}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <section>
+        {todos.length === 0 && (
+          <p>Seção vazia.</p>
+        )}
+        {todos.map(t => (
+          <div key={t.id}>
+            <div>
+              <input
+                type="checkbox"
+                name={t.name}
+                id={`todo-${t.id}`}
+                value={t.name}
+                checked={t.isCompleted}
+                onChange={(event) => handleCompletedTodo(event, t.id)}
+              />
+              <label htmlFor={`todo-checkbox-${t.id}`}>{t.name}</label>
+            </div>
+
+            <button onClick={(event) => handleDeleteTodo(event, t.id)}>Deletar</button>
+          </div>
+        ))}
+      </section>
+    </main>
   )
 }
 
-export default App
+export { App } 
